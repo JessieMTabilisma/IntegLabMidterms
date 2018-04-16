433d717083f7f028cfc18e0488cf1f676d769659
JessieMartinTabilisma <2164631@slu.edu.ph>
2018-04-16 09:17:19 +0800

    added register method/initial main method

--- AdminServer.java
+++ AdminServer.java
@@ -9,16 +9,34 @@ package integlab.IntegLabMidterms;
  *
  * @author jessietabilisma
  */
+import java.io.FileNotFoundException;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
-public class AdminServer {
+public class AdminServer extends UnicastRemoteObject implements Project {
+    
 //    fields
     private Registry registry;
+    private static Scanner usrInput;
+    private static Connection connect;
+    
+    public AdminServer() throws RemoteException {
+     super();   
+    }
 //    Starting the server
     public static String serverStart() throws RemoteException {
-        
+        Project proj = new AdminServer();
+        Registry reg = LocateRegistry.createRegistry(2099);
+        reg.rebind("groupFive", proj);
+        return "Server connection established";
     }
 //    menu
     public static void adminMenu() {
@@ -33,5 +51,45 @@ public class AdminServer {
         System.out.println("[6] Display on goig project/s.");
         System.out.println("[7] Display completed project/s.");
         System.out.println("[8] Exit session.");
+    }
+//    connect database
+    public static void dbConnection(String usr, String pass, String port) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+        String DB_Url = "jdbc:mysql://localhost:"+port+"/rmi_project?user="+usr+"&password="+pass;
+        connect = DriverManager.getConnection(DB_Url);
+        System.out.println("Database connection successfully established");
+        System.out.println("Press ⏎ to continue");
+        usrInput.nextLine();
+        System.out.println("");
+    }
+//    Redister a user
+    public static void registerUser() throws SQLException{
+        System.out.println("Enter username: ");
+        String usrname = usrInput.nextLine();
+        System.out.println("Enter First name: ");
+        String fname = usrInput.nextLine();
+        System.out.println("Enter Last name: ");
+        String lname = usrInput.nextLine();
+        System.out.println("Enter password: ");
+        String pswd = usrInput.nextLine();
+        
+        String query = "INSERT INTO users(usrname, fname, lname, pswd) VALUES(?,?,?,?)";
+        try (PreparedStatement stmt = connect.prepareStatement(query)) {
+            stmt.setString(1, usrname);
+            stmt.setString(2, fname);
+            stmt.setString(3, lname);
+            stmt.setString(4, pswd);
+            
+            int checkRowInserted = stmt.executeUpdate();
+            if(checkRowInserted > 0){
+                System.out.println("New user succesfully registered");
+            }else{
+                System.out.println("Registration failed!");
+            }
+            
+            System.out.println("Press ⏎ to continue");
+            usrInput.nextLine();
+        } catch(SQLException sqlex){
+        }
     }
 }
--- Project.java
+++ Project.java
@@ -15,6 +15,16 @@ import java.io.FileNotFoundException;
 import java.sql.*;
 import java.util.ArrayList;
 public interface Project extends Remote {
-    
+//    public boolean loginUser(String username, String password) throws RemoteException, SQLException;
+//    public boolean validateUserPrivileges(String username, String project_name) throws RemoteException, SQLException;
+//    public ArrayList viewLeaderProjects(String username) throws RemoteException, SQLException;
+//    public String addMember(String username, int project_id) throws RemoteException, SQLException;
+//    public int getProjectID(String project) throws RemoteException, SQLException;
+//    public ArrayList viewProjects(String username) throws RemoteException, SQLException;
+//    public String removeMember(String username, int project_id) throws RemoteException, SQLException;
+//    public String completeProject(int project_id) throws RemoteException, SQLException;
+//    public String uploadFile(String file, String username, int project_id) throws RemoteException, SQLException, FileNotFoundException;
+//    public ArrayList viewAllProjects() throws RemoteException, SQLException;
+//    public ArrayList viewUserProjects(String username) throws RemoteException, SQLException;
 }
  
